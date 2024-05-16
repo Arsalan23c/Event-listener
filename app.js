cff8b42582ec81796c70fe5533172de78a7b6a7d
Arsalan23c <Arsalan23c@gmail.com>
2024-05-16 08:32:55 +0500

    Done

---
+++ app.js
@@ -0,0 +1,37 @@
+var img2 = document.getElementById ('img2')
+img2.addEventListener('mouseover',function(){
+    img1.src ='https://miro.medium.com/v2/resize:fit:1000/1*2c3dB30SysZ8pCtojBTSXg.jpeg'
+})
+img2.addEventListener ('mouseout',function(){
+    img1.src = 'https://wallpapers.com/images/hd/beautiful-place-pictures-4z0hjwoi8rjhlk1p.jpg'
+})
+var img3 = document.getElementById ('img3')
+img3.addEventListener('mouseover',function(){
+    img1.src ='https://publish.purewow.net/wp-content/uploads/sites/2/2024/02/most-beautiful-places-in-the-world_Seljalandsfoss-Iceland.jpg?fit=680%2C400'
+})
+img3.addEventListener ('mouseout',function(){
+    img1.src = 'https://wallpapers.com/images/hd/beautiful-place-pictures-4z0hjwoi8rjhlk1p.jpg'
+})
+
+var img4 = document.getElementById ('img4')
+img4.addEventListener('mouseover',function(){
+    img1.src ='https://blog.thomascook.in/wp-content/uploads/2017/01/fairypools.jpg'
+})
+img4.addEventListener ('mouseout',function(){
+    img1.src = 'https://wallpapers.com/images/hd/beautiful-place-pictures-4z0hjwoi8rjhlk1p.jpg'
+})
+var img5 = document.getElementById ('img5')
+img5.addEventListener('mouseover',function(){
+    img1.src ='https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/05/08123654/lake-como.jpeg'
+})
+img5.addEventListener ('mouseout',function(){
+    img1.src = 'https://wallpapers.com/images/hd/beautiful-place-pictures-4z0hjwoi8rjhlk1p.jpg'
+})
+
+var img6 = document.getElementById ('img6')
+img6.addEventListener('mouseover',function(){
+    img1.src ='https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2023/09/29193141/lake-kawaguchi.jpeg'
+})
+img6.addEventListener ('mouseout',function(){
+    img1.src = 'https://wallpapers.com/images/hd/beautiful-place-pictures-4z0hjwoi8rjhlk1p.jpg'
+})
